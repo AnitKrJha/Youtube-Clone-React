2e1f624cc8fc051cf14119228c9898a220d487fd
Anit Jha <103268398+AnitKrJha@users.noreply.github.com>
2023-07-08 01:55:40 +0530

    Update fetchFromAPI.jsx
    Updated ley

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube-v31.p.rapidapi.com";
 const options = {
   params: { maxResults: "50" },
   headers: {
-    "X-RapidAPI-Key": import.meta.env.VITE_REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'e0aecc6605mshb2ed8845c5858e9p1f9dbajsn9a406d1b16cc',
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
